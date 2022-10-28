2fe63e7d78ce62b5a8ad3a0adf3e2ed88c80c46e
f-evgeniya <f.evgeniya.ef@gmail.com>
2022-10-28 19:14:46 +0300

    Updated forecast days

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ currentDay.innerHTML = formatDate(currentTime);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
